97a3e525bde06b9a33d4d5f0b0d10ecf09fb4503
VSAVIVI <vinoth.savinoth@pearson.com>
2017-02-16 17:36:27 +0530

    Dev Changes

--- config/environments.js
+++ config/environments.js
@@ -8,9 +8,21 @@ module.exports = {
   // NOTE: In development, we use an explicit public path when the assets
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
-  development : (config) => ({
-    compiler_public_path : `http://${config.server_host}:${config.server_port}/`
-  }),
+  // development : (config) => ({
+  //   compiler_public_path : `http://${config.server_host}:${config.server_port}/`
+  // }),
+
+   development : (config) => ({
+    compiler_public_path     : '/',
+    compiler_fail_on_warning : false,
+    compiler_hash_type       : 'chunkhash',
+    compiler_devtool         : null,
+    compiler_stats           : {
+      chunks       : true,
+      chunkModules : true,
+      colors       : true
+    }
+  })
 
   // ======================================================
   // Overrides when NODE_ENV === 'production'
