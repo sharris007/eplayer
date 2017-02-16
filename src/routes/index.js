7c9c31a449f6b65e6d208f014bf2ad9319d348c0
VSAVIVI <vinoth.savinoth@pearson.com>
2017-02-16 18:50:02 +0530

    config changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,9 @@ import bookRoute from './Book';
     PlainRoute objects to build route definitions.   */
 
 export const createRoutes = store => ({
-  path: '/',
+  path: '/eplayer/dist',
   component: CoreLayout,
-  indexRoute: { onEnter: (nextState, replace) => replace('/bookshelf') },
+  indexRoute: { onEnter: (nextState, replace) => replace('/eplayer/dist/bookshelf') },
   childRoutes: [
     bookshelfRoute(store),
     bookRoute(store)
