1176fd8ab761b90db920d8428d674ae2701b2f45
VSAVIVI <vinoth.savinoth@pearson.com>
2017-02-16 18:35:27 +0530

    config changes

--- config/environments.js
+++ config/environments.js
@@ -8,20 +8,8 @@ module.exports = {
   // NOTE: In development, we use an explicit public path when the assets
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
-  // development : (config) => ({
-  //   compiler_public_path : `http://${config.server_host}:${config.server_port}/`
-  // }),
-
   development : (config) => ({
-    compiler_public_path     : '/',
-    compiler_fail_on_warning : false,
-    compiler_hash_type       : 'chunkhash',
-    compiler_devtool         : null,
-    compiler_stats           : {
-      chunks       : true,
-      chunkModules : true,
-      colors       : true
-    }
+    compiler_public_path : `https://pxe-sdk.dev-openclass.com/eplayer/dist/`
   }),
 
   // ======================================================
