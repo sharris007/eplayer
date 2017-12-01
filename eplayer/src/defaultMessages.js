ddc821efab1eb22db16f2cd045dc52b1e4cf47f5
Saravanakumar <saravanakumar.kathirvel@pearson.com>
2017-12-01 18:00:46 +0530

    Included request payload

---
+++ eplayer/src/defaultMessages.js
@@ -0,0 +1,85 @@
+import { defineMessages } from 'react-intl';
+
+//
+// Default messages are 'en-US'
+//
+const messages = defineMessages({
+  backNavigation: {
+    id: 'backNavigation',
+    defaultMessage: 'Back Navigation'
+  },
+  hamburgerMenu: {
+    id: 'hamburgerMenu',
+    defaultMessage: 'Hamburger Menu'
+  },
+  preferenceIcon: {
+    id: 'preferenceIcon',
+    defaultMessage: 'Preference'
+  },
+  searchIcon: {
+    id: 'searchIcon',
+    defaultMessage: 'Search'
+  },
+  volumeIcon: {
+    id: 'volumeIcon',
+    defaultMessage: 'Volume'
+  },
+  Glossary: {
+    id: 'Glossary',
+    defaultMessage: 'Glossary'
+  },
+  glossaryTerm: {
+    id: 'Glossary Terms',
+    defaultMessage: 'Glossary Terms'
+  },
+  Footnotes: {
+    id: 'Footnotes',
+    defaultMessage: 'Footnotes'
+  },
+  logOut: {
+    id: 'logOut',
+    defaultMessage: 'Log Out'
+  },
+  clickToLogout: {
+    id: 'clickToLogout',
+    defaultMessage: 'Click To Log Out'
+  },
+  pearsonLogo: {
+    id: 'pearsonLogo',
+    defaultMessage: 'Pearson Logo'
+  },
+  myBookshelf: {
+    id: 'myBookshelf',
+    defaultMessage: 'My Bookshelf'
+  },
+  revealAnswer: {
+    id: 'revealAnswer',
+    defaultMessage: 'Reveal Answer'
+  },
+  chaptertitle: {
+    id: 'chaptertitle',
+    defaultMessage: 'Chapter'
+  },
+  content: {
+    id: 'content',
+    defaultMessage: 'Content'
+  },
+  pagetitle: {
+    id: 'pagetitle',
+    defaultMessage: 'Page'
+  },
+  'learningobjectives.authoredtext': {
+    id: 'learningobjectives.authoredtext',
+    defaultMessage: 'Learning Objectives'
+  },
+  'glossary.text': {
+    id: 'glossary.text',
+    defaultMessage: 'Glossary'
+  },
+  'glossary.word': {
+    id: 'glossary.word',
+    defaultMessage: 'Glossary'
+  }
+});
+
+export default messages;
--- eplayer/src/routes/ETBook/modules/searchActions.js
+++ eplayer/src/routes/ETBook/modules/searchActions.js
@@ -1,12 +1,54 @@
 import { resources, domain, typeConstants } from '../../../../const/Settings';
+import message from '../../../defaultMessages';
 
-let searchUrl = resources.links.etextSearchUrl[domain.getEnvType()];
 const searchFilters = {
   indexType: 'nextcontent',
   fieldsToReturn: ['chaptertitle', 'pagetitle', 'learningobjectives', 'content', 'type', 'glossary', 'figure'],
   fields: ['chaptertitle', 'glossary.word', 'glossary.text', 'learningobjectives.authoredtext', 'pagetitle'],
   highlightFields: ['chaptertitle', 'glossary.word', 'glossary.text', 'pagetitle', 'learningobjectives.authoredtext', 'content', 'figure.title']
 };
+
+const payLoad = { 
+  "queryString":"",
+  "indexType":"nextcontent",
+  "filter":["indexid:"], 
+  "responseSize":50,
+  "searchOnMultipleIndexes":"true",
+  "searchType":"FACET",
+  "groupBy":["_index"]
+};
+
+function keyIndex(arr, key) {
+  return arr.findIndex(item => (key === item.category));
+}
+function searchTitle(titles, key) {
+  return titles[key.toLocaleLowerCase()].id === key ? titles[key.toLocaleLowerCase()].defaultMessage : key;
+}
+function getSearchFormat(response) {
+  const searchResults = [];
+  if (response) {
+    response.forEach((result) => {
+      searchFilters.fields.forEach((key) => {
+        const titles = message;
+        if (key in result.matchedFields) {
+          let obj = {};
+          const title = key in titles ? searchTitle(titles, key) : key;
+          const categoryIndex = keyIndex(searchResults, title);
+          if (categoryIndex < 0) {
+            obj.category = title;
+            obj.results = [{ id: result.id, content: result.matchedFields[key].replace('[', '').replace(']', '') }];
+            searchResults.push(obj);
+          } else {
+            obj = searchResults[categoryIndex];
+            obj.results.push({ id: result.id, content: result.matchedFields[key].replace('[', '').replace(']', '') });
+          }
+        }
+      });
+    });
+    return searchResults;
+  }
+  return searchResults;
+}
 
 const searchActions = {
 
@@ -22,9 +64,11 @@ const searchActions = {
     };*/
   },
   autoComplete(searchcontent, handleResults) {
-    searchFilters.filter = [window.localStorage.getItem('searchIndexId')];
+    /*searchFilters.filter = [window.localStorage.getItem('searchIndexId')];
     searchFilters.queryString = searchcontent;
-    searchFilters.responseSize = 6;
+    searchFilters.responseSize = 6;*/
+    payLoad.queryString = searchcontent;
+    payLoad.filter[0] += window.localStorage.getItem('searchIndexId');
     console.log(resources.links.etextSearchUrl[domain.getEnvType()]);
     fetch(resources.links.etextSearchUrl[domain.getEnvType()], 
       {
@@ -33,10 +77,11 @@ const searchActions = {
           'application-id': 'ereader',
           'Content-Type': 'application/json',
           'X-Authorization': localStorage.getItem('secureToken')
-        }
+        },
+        body: JSON.stringify(payLoad)
       }).then(response => response.json())
      .then((response) => {
-       handleResults(response)
+       handleResults((getSearchFormat(response)));
      })
     /*return {
       type: 'SEARCH',
