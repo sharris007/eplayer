90f0a5550cb5b355105e9e5bf681c659c878e870
Saravanan <saravanan.kandasamy@pearson.com>
2018-01-23 18:15:01 +0530

    unit testing code setup

---
+++ eplayer/tests/eT2UnitTestCases/ETBook/Book.spec.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import BookshelfPage from '../../../src/routes/ETBook/components/Book.js'
+
+describe('EtBookshelf Bookshelf component', () => {
+    it('Bookshelf should render as expected', () => {
+        
+        console.log("Success");
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe("Bookshelf Component renders", function(){
+		
+
+// 		it('Should render as', () => {
+//     		console.log("success");
+//   		})
+
+  	
+
+// 	})
